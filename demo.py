c12ef4cf1f70a02eab99a3f2023abf541c1ef55c
Aiden Grossman <aidengrossman@google.com>
2024-10-21 02:44:56 +0000

    Formatting

--- demo.py
+++ demo.py
@@ -10,14 +10,19 @@ OPT_PATH = 'opt'
 
 total_instruction_count = []
 
-for bc_index in range(0,100):
-  bc_contents = next(bc_dataset_iter)['content']
-  command_vector = ['/tmp/opt', '-passes=print<func-properties>', '-enable-detailed-function-properties', '-', '-disable-output']
-  result = subprocess.run(command_vector, input=bc_contents, capture_output=True)
-  for line in result.stderr.decode('utf-8').split('\n'):
-    if line.startswith('TotalInstructionCount'):
-      total_instruction_count.append(int(line.split('TotalInstructionCount: ')[1]))
-      break
+for bc_index in range(0, 100):
+    bc_contents = next(bc_dataset_iter)['content']
+    command_vector = [
+        '/tmp/opt', '-passes=print<func-properties>',
+        '-enable-detailed-function-properties', '-', '-disable-output'
+    ]
+    result = subprocess.run(command_vector,
+                            input=bc_contents,
+                            capture_output=True)
+    for line in result.stderr.decode('utf-8').split('\n'):
+        if line.startswith('TotalInstructionCount'):
+            total_instruction_count.append(
+                int(line.split('TotalInstructionCount: ')[1]))
+            break
 
 print(total_instruction_count)
-
